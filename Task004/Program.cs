d2e61dfedf7145c0387381c1bbbdce14450489a4
Leonid <leeonidix@gmail.com>
2022-07-09 12:16:40 +0500

    Task004 done

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+Console.Write("Enter the number: ");
+int num = int.Parse(Console.ReadLine());
+int i = 1;
+while(i<=num)
+{
+    if (i%2==0)
+    {
+        Console.Write($"{i} ");
+        i++;
+    }
+    else
+    {
+         i++;
+    }
+}
