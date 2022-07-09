f5bb4761fa1dd7819af52c1b4d63bf748c185dec
Leonid <leeonidix@gmail.com>
2022-07-09 11:51:25 +0500

    Task002 Done on 1/2

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.Write("Enter first number: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int numB = int.Parse(Console.ReadLine());
+Console.Write("Enter third number: ");
+int numC = int.Parse(Console.ReadLine());
+int max = numA;
+if(max < numB)
+{
+    max = numB;
+    Console.WriteLine($"{numA}, {numB}, {numC} -> {max}");
+}
+else if(max < numC) // НЕ работает Почему??
+{
+    max = numC;
+    Console.WriteLine($"{numA}, {numB}, {numC} -> {max}");
+}
+else
+    Console.WriteLine($"{numA}, {numB}, {numC} -> {max}");
