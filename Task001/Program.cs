e9214c1e2dbbf13b4961efbdcc7e41017ffb2974
Leonid <leeonidix@gmail.com>
2022-07-09 11:29:12 +0500

    Task001 Done

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт максимальное из этих чисел.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.Write("Enter first number: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int numB = int.Parse(Console.ReadLine());
+int max = numA;
+if(numA<numB)
+{
+    max = numB;
+    Console.WriteLine($"a = {numA}, b = {numB} -> max = {max}");
+}
+else
+{
+    Console.WriteLine($"a = {numA}, b = {numB} -> max = {max}");
+}
